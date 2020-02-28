9bbfdb74232cbccd244930661f9b60e0af0f3458
Adriano <adriano.m.m1@gmail.com>
2020-02-28 10:01:55 -0300

    Configuracao

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'mudar123',
+  database: 'menucase',
+  dfine: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
